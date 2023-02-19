52c54cd64ace8e3b66486abce5dae41adac426a8
Noah3141 <66894106+Noah3141@users.noreply.github.com>
2023-02-19 16:56:30 -0500

    Add files via upload

---
+++ Russian-VocabLister/input_vocabulary_compiler.py
@@ -0,0 +1,12 @@
+# This file supposed to take:
+# 1) WebCrawler's master list (RUBIT'ed)
+# 2) Input from user from website (RUBIT'ed)
+# and create an output file that:
+# 1) follows the settings from the site (broad, full, tree, pairs)
+# 2) creates a table with:
+    # A) entry word from input
+    # B) frequency in input
+    # C) indicator(s) of frequency in master list
+    # D) culled entries by breadth setting
+    # E) grouped/ordered entries by Output_Style setting 
+
